9d64f73e7d7c3acd8908f0a92100455d62c8098b
Michelle Choi <43436945+ycho0005@users.noreply.github.com>
2020-08-18 22:16:28 +1000

    Update PlayingCardImpl.java
    Misch Push: playing card class updated with all the getter and setter

--- CardGame/CardGame/src/model/PlayingCardImpl.java
+++ CardGame/CardGame/src/model/PlayingCardImpl.java
@@ -1,30 +1,46 @@
 package model;
 
 import model.interfaces.PlayingCard;
+import model.interfaces.PlayingCard.Suit;
+import model.interfaces.PlayingCard.Value;
 
 public class PlayingCardImpl implements PlayingCard{
+
+	 Suit CardSuit;
+	 Value CardValue;
+	 int score;
+
+	public PlayingCardImpl(String suit, String value) {
+		//constructor should create array/array list or some sort of object that depicts 28 card shuffled
+		this.CardSuit = Suit.valueOf(suit);
+		this.CardValue = Value.valueOf(value);
+		this.score = 0;		
+	}
 
 	@Override
 	public Suit getSuit() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.CardSuit;
 	}
 
 	@Override
 	public Value getValue() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.CardValue;
 	}
 
 	@Override
 	public int getScore() {
 		// TODO Auto-generated method stub
-		return 0;
+		return score;
 	}
 
 	@Override
 	public boolean equals(PlayingCard card) {
 		// TODO Auto-generated method stub
+		if(this.score == card.getScore()) {
+			return true;
+		}
 		return false;
 	}
 
